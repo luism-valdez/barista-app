dc182d6627d9d832960c34b71f5d36148253e47d
luism-valdez <156615555+luism-valdez@users.noreply.github.com>
2024-10-08 18:25:26 -0700

    added the barista and recipe choice components

--- src/Components/BaristaForm.jsx
+++ src/Components/BaristaForm.jsx
@@ -1,4 +1,6 @@
 import React, {Component, useState} from "react";
+import RecipeChoices from "./RecipeChoices";
+
 const BaristaForm = () => {
 
     const [inputs, setInputs] = useState({
@@ -6,7 +8,7 @@ const BaristaForm = () => {
         'milk': '',
         'syrup': '',
         'blended': ''
-    });
+    })
     const ingredients = {
         'temperature' : ['hot', 'lukewarm', 'cold'],
         'syrup': ['mocha', 'vanilla', 'toffee', 'maple', 'caramel', 'other', 'none'],
@@ -26,7 +28,22 @@ const BaristaForm = () => {
 
     <div>
         <h2>Hi, I'd like to order a:</h2>
-        <form></form>
+
+        <form>
+            <h3>Temperature</h3>
+            <div className="answer-space" >
+             {inputs["temperature"]} 
+            </div>
+            <RecipeChoices
+                handleChange={(e) => setInputs((prevState) => ({
+                    ...prevState,
+                    [e.target.name]: e.target.value,
+                }))}
+                label="temperature"
+                choices={ingredients["temperature"]}
+                checked={inputs["temperature"]}
+            />
+        </form>
         <button type="submit" className="button submit" onClick={onCheckAnswer}>Check Answer</button>
         <button type="new-drink-button" className="button submit" onClick={onNewDrink}>New Drink</button>
     </div>
